dd561753e9f943a4fc1c074371a1cda888ae9963
migueXYLO <2221462@my.ipleiria.pt>
2024-03-21 13:54:28 +0000

    adoro ainet, jurooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo

---
+++ public/my_functions.php
@@ -0,0 +1,17 @@
+<?php
+function courseFullName(string $name, string $type)
+{
+    if (strtolower($type) == 'master') {
+        return "Master's in " . $name;
+    } elseif (strtolower($type) == 'tesp') {
+        return 'TeSP - ' . $name;
+    }
+    return $name;
+}
+
+function filterDisciplines(array $disciplines, int $year, int $semester): array
+{
+    return array_values(array_filter($disciplines, function ($value) use ($year, $semester) {
+        return $value->year == $year && $value->semester == $semester;
+    }));
+}
